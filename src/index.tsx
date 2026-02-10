b7348176067656c994ca556dac90fd38e6c113f3
markpivottai <mark@pivottai.com>
2026-02-10 21:49:11 +0800

    Update and rename index.tsx to src/index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css';  // ← ADD THIS LINE
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
